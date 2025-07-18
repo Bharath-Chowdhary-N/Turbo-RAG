d1a406c79059605a49adb8ebd23f46f1e2ff03dd
Bharath-Chowdhary-N <n.bharath.chowdhary@gmail.com>
2025-07-18 18:25:04 -0500

    added main function

---
+++ main.py
@@ -0,0 +1,9 @@
+from repo_processor import repo_processor
+
+def main():
+    #Start repo processing
+    repo_processor
+
+if __name__ == "__main__":
+    main()
+    
--- repo_processor.py
+++ repo_processor.py
@@ -3,13 +3,23 @@
 
 import os
 import git
+import shutil
 
 class repo_processor():
     def __init__(self):
         self.target_repo = "https://github.com/patkel/turbo_telescope" #Address
+        self.clone_path = "home/bcn/Work/PostDoc/"
         self.clone_location = "home/bcn/Work/PostDoc/turbo_telescope-master"  #clone location
 
 
         # Download repo is it does not exist
-        self.download_repo()
+        self.clone_repo()
+    
+    def clone_repo(self):
+        if os.path.exists(self.clone_location):
+            shutil.rmtree(self.clone_location)
+        print("Processing cloning repo: {} to : {}".format(self.target_repo), self.clone_path)
+        git.Repo.clone_from(self.clone_repo,self.clone_location)
+        print("!!!Cloning Done!!!!")
+
     
